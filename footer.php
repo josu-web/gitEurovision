72a9bca2b8d2c8008338306bd1e1fe988e490701
josu <josusanchez.e@gmail.com>
2021-03-14 09:39:13 +0100

    Primera Version

--- footer.php
+++ footer.php
@@ -18,3 +18,61 @@
     <script src= "js/jsFooter.js?ver=1.8"></script>
    
         
+    <footer>
+        
+           
+          
+          
+            <div class="tit2Footer">Esta web se trata de una simulación, de un ejercicio personal sin ningún interés económico ni vinculación alguna con ninguna entidad, realizada únicamente a nivel personal para seguir investigando y practicando mis conocimientos del desarrollo web.</div>
+         
+           
+           <div id="ic">
+             <a href="#ini"><i id="iconoInt2" class="fas fa-arrow-alt-circle-up fa-3x"></i></a>
+           </div>
+        
+    
+    </footer>
+    
+    <script src= "https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src= "js/jsFooter.js?ver=1.8"></script>
+   
+
+
+    <footer>
+        
+           
+          
+          
+            <div class="tit2Footer">Esta web se trata de una simulación, de un ejercicio personal sin ningún interés económico ni vinculación alguna con ninguna entidad, realizada únicamente a nivel personal para seguir investigando y practicando mis conocimientos del desarrollo web.</div>
+         
+           
+           <div id="ic">
+             <a href="#ini"><i id="iconoInt2" class="fas fa-arrow-alt-circle-up fa-3x"></i></a>
+           </div>
+        
+    
+    </footer>
+    
+    <script src= "https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src= "js/jsFooter.js?ver=1.8"></script>
+   
+
+
+    <footer>
+        
+           
+          
+          
+            <div class="tit2Footer">Esta web se trata de una simulación, de un ejercicio personal sin ningún interés económico ni vinculación alguna con ninguna entidad, realizada únicamente a nivel personal para seguir investigando y practicando mis conocimientos del desarrollo web.</div>
+         
+           
+           <div id="ic">
+             <a href="#ini"><i id="iconoInt2" class="fas fa-arrow-alt-circle-up fa-3x"></i></a>
+           </div>
+        
+    
+    </footer>
+    
+    <script src= "https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src= "js/jsFooter.js?ver=1.8"></script>
+   
